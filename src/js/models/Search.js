1b1d220181bf62e6d18989cf0f4db37757113791
Khanh Linh Nguyen <sun.79997@gmail.com>
2020-07-21 23:01:36 +0300

    node_modules be gone!

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -7,9 +7,12 @@ export default class Search{
 
     async getResults() {
         try {
-            const API = `https://forkify-api.herokuapp.com/api/search?q=${this.query}`;
+            const apiKey = `2af065936b4f467aafaf716a555b52d0`; //Spoonacular API
+            const API = `https://forkify-api.herokuapp.com/api/search?q=${this.query}`; //Forkify API
+            //const API = `https://api.spoonacular.com/recipes/search?apiKey=${apiKey}&query=${this.query}`; //Spoonacular API
             const res = await axios(API);
-            this.result = res.data.recipes;
+            this.result = res.data.recipes; //Forkify API
+            // this.result = res.data.results; //Spoonacular API
             //console.log(this.result);
         } catch (error){
             alert(error + ' Sorry! There are no recipes for what you searched :(');
